0b9a3dfb8f1fb54b05023b21a66a481de3dcc756
pololover <pololover>
2022-07-19 10:07:46 +0900

    FEAT: 이벤트 설정

--- FirstNewApp/ViewController.swift
+++ FirstNewApp/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    func configureUI() {
+        mainLabel.text = "초를 선택하세요"
     }
 
 
+    @IBAction func sliderChanged(_ sender: UISlider) {
+    }
+    
+    
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
